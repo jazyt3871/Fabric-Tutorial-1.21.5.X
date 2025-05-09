0cd60b88a91a55d48b9e4a5185b6748934ec16dc
jasper-schepers <jasper.schepers@delifteducation.be>
2025-05-09 22:26:31 +0200

    Initial commit

---
+++ src/main/java/com/jaz/tutorialmod/TutorialModCLient.java
@@ -0,0 +1,10 @@
+package com.jaz.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModCLient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
